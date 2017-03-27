1b145f1dd527982b4f8c66130e68e538d4b1873d
zhoumingkai <632549546@qq.com>
2017-03-27 20:35:21 +0800

    Initial commit

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * V1.0.0
+ * Created by zhoumingkai on 2017/3/27.
+ */
+public class TestDemo {
+    @Test
+    public void testcase1() {
+        Assert.assertTrue(false);
+        System.out.println("testcase1");
+    }
+
+    @Test
+    public void testcase2() {
+        Assert.assertTrue(true);
+        System.out.println("testcase2");
+    }
+}
+
